5a390ceb0107e32b041290b2336bedd492770918
bavariah <48217024+bavariah@users.noreply.github.com>
2020-07-14 15:02:36 +0200

    slide3

--- js/myScript.js
+++ js/myScript.js
@@ -33,9 +33,13 @@
 // var slideIndex = 0;
 // showSlides();
 
+var slideIndex = 0;
+showSlides();
+
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -43,6 +47,10 @@ function showSlides() {
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
