d882255d8cdd69a1f8e4365e467bbbebaa38e287
bavariah <48217024+bavariah@users.noreply.github.com>
2020-07-14 14:53:15 +0200

    slide2

--- js/myScript.js
+++ js/myScript.js
@@ -1,15 +1,15 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
 
 // function showSlides(n) {
 //   var i;
